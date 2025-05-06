ce401a0c2c7daa7275310286cf69766e1b39ff50
Mvteiio <romancamargo02@gmail.com>
2025-05-06 08:41:05 -0500

    Update :fire: Doing Functions

--- Dia13/scripts/script.js
+++ Dia13/scripts/script.js
@@ -17,7 +17,17 @@ function ponerUsuarios(data) {
             const nuevaTarea = template.cloneNode(true);
             nuevaTarea.style.display = "flex";
             nuevaTarea.querySelector("#nombreTarea").textContent = tarea["task"];
+
+            nuevaTarea.querySelector("#chulito").setAttribute("data-id", tarea["id"]);
+            nuevaTarea.querySelector("#chulito").addEventListener("click", realizado);
+
+            nuevaTarea.querySelector("#eliminar").setAttribute("data-id", tarea["id"]);
+            nuevaTarea.querySelector("#eliminar").addEventListener("click", eliminar);
+
             nuevasTareas.appendChild(nuevaTarea);
+            
+        
+
         } 
         else if (tarea["status"] === "ready") {
             const nuevaTarea = template.cloneNode(true);
@@ -28,15 +38,60 @@ function ponerUsuarios(data) {
             nombreTarea.style.textDecoration = "line-through";
             nombreTarea.style.opacity = "0.5";
 
-            nuevaTarea.querySelector("#nombreTarea").textContent = tarea["task"];
+            nuevaTarea.querySelector("#nombreTarea").   textContent = tarea["task"];
+
+            nuevaTarea.querySelector("#chulito").setAttribute("data-id", tarea["id"]);
+            nuevaTarea.querySelector("#chulito").addEventListener("click", realizado);
+
+            nuevaTarea.querySelector("#eliminar").setAttribute("data-id", tarea["id"]);
+            nuevaTarea.querySelector("#eliminar").addEventListener("click", eliminar);
+
             nuevasTareas.appendChild(nuevaTarea);
+            
+            
         }
     });
 }
 
-function cambiarStatus (){
-    let realizado = document.getElementById("chulito")
+async function realizado(evento){
+    let id = evento.currentTarget.getAttribute("data-id");
+    console.log(id);
+    await fetch(`https://6814210f225ff1af1627f7de.mockapi.io/api/v1/tareas/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            status: "ready"
+        })
+    });
+    llamadaAPI();
+}
 
+async function eliminar(evento){
+    let id = evento.currentTarget.getAttribute("data-id");
+    console.log(id);
+    await fetch(`https://6814210f225ff1af1627f7de.mockapi.io/api/v1/tareas/${id}`, {
+        method: "DELETE",
+    });
+    llamadaAPI();
+}
+
+async function addTask(){
+    let task = document.getElementById("text")
+    let valorTexto = task.value
+
+    await fetch(`https://6814210f225ff1af1627f7de.mockapi.io/api/v1/tareas/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            task: valorTexto,
+            status: "On hold"
+        })
+    });
+    llamadaAPI();
 }
 
 
